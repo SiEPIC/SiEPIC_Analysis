beaf3eaa4c9e84329a03698534beb84a8c4c3ca4
Trufflechoco <annet6280@gmail.com>
2023-10-19 11:35:12 -0700

    Initial commit

---
+++ analysis_packageV2/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for V2 Analysis Package."""
+
+__author__ = """Tenna Yuan"""
+__email__ = 'tenna@student.ubc.ca'
+__version__ = '0.1.0'
+
+from analysis_packageV2.device import Device
+
+__all__ = ['Device']
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='analysis_packageV2',
+    version='0.1',
+    description='description to be filled',
+    author='Tenna Yuan',
+    author_email='tenna@student.ubc.ca',
+    packages=find_packages(),
+)
