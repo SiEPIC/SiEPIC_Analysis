da8e66c3f5e709ad0812d6440c7f45f337b49b73
Trufflechoco <annet6280@gmail.com>
2023-11-04 22:32:08 -0700

    Updated file saving parameters

---
+++ analysis_package/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for V2 Analysis Package."""
+
+__author__ = """Tenna Yuan"""
+__email__ = 'tenna@student.ubc.ca'
+__version__ = '0.1.0'
+__all__ = ['Device', 'Execute']
+
+from analysis_package.device import Device
+from analysis_package.execute import Execute
