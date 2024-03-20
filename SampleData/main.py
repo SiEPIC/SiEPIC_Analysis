007438d0214cd1ad64d7b9e2285c964c3d0c9af8
Trufflechoco <annet6280@gmail.com>
2024-03-20 16:13:16 -0700

    Examples updated

--- SampleData/main.py
+++ SampleData/main.py
@@ -5,13 +5,13 @@ from analysis_package import Execute
 if __name__ == "__main__":
     all_reports = []
 
-    root_path = r"D:\Documents\Uni\Year_4COOP\Docs\Analysis_Scripts\package\SampleData\data\1550_TE"
+    root_path = os.path.join(os.getcwd(),'1550_TE')
     analyzer_1 = Execute(root_path)
     results_df_1, report_path = analyzer_1.gen_analysis()
     print(results_df_1)
     all_reports.append(report_path)
 
-    root_path = r"D:\Documents\Uni\Year_4COOP\Docs\Analysis_Scripts\package\SampleData\data\1550_TM"
+    root_path = os.path.join(os.getcwd(),'1550_TM')
     analyzer_2 = Execute(root_path)
     results_df_2, report_path = analyzer_2.gen_analysis()
     print(results_df_2)
