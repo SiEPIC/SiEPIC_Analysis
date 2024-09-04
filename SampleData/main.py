f289ddd87b7c453606f95944afeb416d6b558c80
Mustafa Hammood <mustafa@siepic.com>
2024-09-04 09:01:01 -0700

    sample data results

--- SampleData/main.py
+++ SampleData/main.py
@@ -1,7 +1,6 @@
 import os
 from analysis_package import Execute
 
-
 # Example usage:
 if __name__ == "__main__":
     all_reports = []
@@ -16,20 +15,28 @@ if __name__ == "__main__":
     results_df_1, report_path = analyzer_1.gen_analysis()
     print(results_df_1)
     all_reports.append(report_path)
-    """
-    
-    analyzer_1 = Execute(root_path)
-    results_df_1, report_path = analyzer_1.gen_analysis()
-    print(results_df_1)
-    all_reports.append(report_path)
 
-    root_path = os.path.join(os.getcwd(),'1550_TM')
-    analyzer_2 = Execute(root_path)
+    chip_name = 'EdX_May_2023_1310TE'
+    root_path = os.path.join(os.getcwd(), '1310_TE')
+    analyzer_2 = Execute(root_path, chip_name, measure_date, process)
     results_df_2, report_path = analyzer_2.gen_analysis()
     print(results_df_2)
+    all_reports.append(report_path)
+
+    chip_name = 'EdX_May_2023_1550TM'
+    root_path = os.path.join(os.getcwd(), '1550_TM')
+    analyzer_3 = Execute(root_path, chip_name, measure_date, process)
+    results_df_3, report_path = analyzer_3.gen_analysis()
+    print(results_df_3)
+    all_reports.append(report_path)
+
+    chip_name = 'EdX_May_2023_1310TM'
+    root_path = os.path.join(os.getcwd(), '1310_TM')
+    analyzer_4 = Execute(root_path, chip_name, measure_date, process)
+    results_df_4, report_path = analyzer_4.gen_analysis()
+    print(results_df_4)
     all_reports.append(report_path)
 
     # Combine the PDFs after all runs if needed
     combined_pdf_path = os.path.join(os.path.dirname(root_path), "analysis_results", "combined_results.pdf")
     analyzer_1.merge_pdfs(all_reports, combined_pdf_path)
-    """
